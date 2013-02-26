87bfcc8c7eebf602b7941be3671aa767f2e55081
Joe Bentley <joebentley10@gmail.com>
2013-02-26 19:37:20 +0000

    Particle engine destroy added.
    Added a check for when all particles are disabled, at this point the
    engine will destroy itself to save precious resources.

--- src/components.js
+++ src/components.js
@@ -163,7 +163,7 @@ Crafty.c('Particles', {
 	},
 
 	particles: function (x, y, timetolivelow, timetolivehigh, speed, duration) {
-		// Create our own canvas to draw on
+		// Create our own canvas to draw on, set the size and add it to the stage
 		var c, ctx;
 
 		c = document.createElement("canvas");
@@ -175,16 +175,15 @@ Crafty.c('Particles', {
 
 		ctx = c.getContext('2d');
 
-		// Set up default particle
+		// Set up particle and params
 		this._Particles.particle.x = x;
 		this._Particles.particle.y = y;
-
 		this._Particles.timetolivelow = timetolivelow;
 		this._Particles.timetolivehigh = timetolivehigh;
 		this._Particles.speed = speed;
 		this._Particles.duration = duration;
 
-		// Clean up the DOM when this component is removed
+		// Destroy the particle engine when the component is removed or destroyed
 		this.bind('Remove', function () {
 			Crafty.stage.elem.removeChild(c);
 		}).bind("RemoveComponent", function (id) {
@@ -201,6 +200,19 @@ Crafty.c('Particles', {
 			
 			// Render them!
 			this._Particles.render(ctx);
+
+			// Loop through each particle, if any of them are still enabled, 
+			// keep running the engine, else destroy it to save resources
+			var alive = false;
+			for (var i = 0; i < this._Particles.particles.length; i++) {
+				if (this._Particles.particles[i].enabled == true) {
+					alive = true;
+					break;
+				}
+			};
+			if (!alive) {
+				this.destroy();
+			}
 		});
 	},
 
@@ -208,15 +220,19 @@ Crafty.c('Particles', {
 		// Our list of particles
 		particles: [],
 
+		// Current frame
 		frame: 0,
 
+		// Whether the particle engine should keep generating particles
 		running: true,
 
+		// Params
 		timetolivelow: 0,
 		timetolivehigh: 0,
 		speed: 0,
 		duration: 0,
 
+		// Object to create a particle from
 		particle: {
 			x: 0,
 			y: 0,
@@ -228,8 +244,12 @@ Crafty.c('Particles', {
 		},
 
 		update: function (frame) {
+			// If the particle engine is running, start generating new particles
 			if (this.running) {
-				// Push new particle into particles
+				/* Generate some particles... add five particles this frame
+				 * Build an object with random angle and random timetolive
+				 * between the two boundaries, and generate a random color
+				 * then push this into our array of particles */
 				for (var i = 0; i < 5; i++) {
 					var newParticle = Object.create(this.particle);
 					newParticle.angle = Crafty.math.randomNumber(0, 2 * Math.PI);
@@ -239,23 +259,29 @@ Crafty.c('Particles', {
 				};
 			}
 
+			// Update the position of each particle in the list
 			for (var i = 0; i < this.particles.length; i++) {
+				// Use trig and the particles angle to calculate how much to change the (x, y) by each frame
 				this.particles[i].x += this.speed * Math.sin(this.particles[i].angle);
 				this.particles[i].y += this.speed * Math.cos(this.particles[i].angle);
+
+				/* Increment the number of frames this particle has been alive for, if this is greater
+				 * than the particle's timetolive, disable the particle from being rendered */
 				this.particles[i].frame++;
 				if (this.particles[i].frame >= this.particles[i].timetolive) {
 					this.particles[i].enabled = false;
 				}
 			};
 
+			// Disable the particle engine if duration has been reached
 			if (this.frame >= this.duration) {
 				this.running = false;
 			}
-
 			this.frame++;
 		},
 
 		render: function (ctx) {
+			// Render each particle
 			for (var i = 0; i < this.particles.length; i++) {
 				if (this.particles[i].enabled) {
 					ctx.fillStyle = this.particles[i].color;
